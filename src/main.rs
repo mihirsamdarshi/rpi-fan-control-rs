10af00dba29d5a6eb8b2c59b962b52f483c759e0
Mihir Samdarshi <msamdars@stanford.edu>
2022-11-13 17:49:26 -0800

    feat: remove sysinfo dependency, add more detailed error messages

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::{f32::consts::PI, io::ErrorKind};
 
 use rppal::pwm::{Channel, Polarity, Pwm};
-use sysinfo::{ComponentExt, System, SystemExt};
 
 const PWM_FREQUENCY: f64 = 25_000.0;
 /// [°C] temperature below which to stop the fan
@@ -16,7 +15,7 @@ const FAN_LOW: f32 = 0.1;
 const FAN_MAX: f32 = 1.0;
 const FAN_GAIN: f32 = (FAN_MAX - FAN_LOW) / (MAX_TEMP - MIN_TEMP);
 
-const UDEV_RULES: &str = r#"
+const UDEV_ERROR: &str = r#"
 As of kernel version 4.14.34, released on April 16 2018, it's possible to configure your Raspberry Pi to allow non-root access to PWM. 
 4.14.34 includes a patch that allows udev to change file permissions when a PWM channel is exported. 
 This will let any user that is a member of the GPIO group configure PWM without having to use sudo.
@@ -33,22 +32,29 @@ SUBSYSTEM=="pwm*", PROGRAM="/bin/sh -c '\
 ```
 "#;
 
+const PERMISSION_ERROR: &str = r#"
+By default, both channels are disabled.
+
+To enable only PWM0 on its default pin (BCM GPIO 18, physical pin 12), add dtoverlay=pwm to /boot/config.txt on Raspberry Pi OS or boot/firmware/usercfg.txt on Ubuntu.
+If you need both PWM channels, replace pwm with pwm-2chan, which enables PWM0 on BCM GPIO 18 (physical pin 12), and PWM1 on BCM GPIO 19 (physical pin 35).
+More details on enabling and configuring PWM on other GPIO pins than the default ones can be found in /boot/overlays/README.
+"#;
+
 /// Returns the temperature of the CPU in degrees Celsius.
-fn get_cpu_temp(sys: &mut System) -> f32 {
-    sys.refresh_components();
-
-    for component in sys.components() {
-        println!("{:?}", component);
-    }
-
-    let cpu_temp = sys
-        .components()
-        .iter()
-        .map(|s| s.temperature())
-        .next()
-        .unwrap_or(0.0);
-
-    cpu_temp
+fn get_cpu_temp() -> f32 {
+    let temp_unparsed = match std::fs::read_to_string("/sys/class/thermal/thermal_zone0/temp") {
+        Ok(temp) => temp,
+        Err(e) => match e.kind() {
+            ErrorKind::PermissionDenied => {
+                panic!("Failed to read /sys/class/thermal/thermal_zone0/temp.")
+            }
+            ErrorKind::NotFound => {
+                panic!("No temperature sensor found. Make sure you're running on a Raspberry Pi.")
+            }
+            _ => "45000".to_string(),
+        },
+    };
+    temp_unparsed.trim().parse::<f32>().unwrap_or(45000.0) / 1000.0
 }
 
 fn fan_curve(temp: f32) -> f32 {
@@ -72,32 +78,37 @@ fn handle_fan_speed(cpu_temp: f32, pwm: &mut Pwm) -> Result<(), std::io::Error> 
 }
 
 fn main() {
-    let mut sys = System::new();
-
     let mut pwm_pin =
         match Pwm::with_frequency(Channel::Pwm0, PWM_FREQUENCY, 0.0, Polarity::Normal, true) {
             Ok(pin) => pin,
             Err(rppal::pwm::Error::Io(e)) => match e.kind() {
                 ErrorKind::PermissionDenied => {
                     eprintln!(
-                        "{}",
-                        format!(
-                            "Make sure /sys/class/pwm and all of its subdirectories are owned by \
-                             root:gpio, the current user is a member of the gpio group, and udev \
-                             is properly configured as mentioned below. Alternatively, you can \
-                             launch your application using sudo.\n\n{}",
-                            UDEV_RULES
-                        )
+                        "{}\n\n{}",
+                        "Make sure /sys/class/pwm and all of its subdirectories are owned by \
+                         root:gpio, the current user is a member of the gpio group, and udev is \
+                         properly configured as mentioned below. Alternatively, you can launch \
+                         your application using sudo.",
+                        UDEV_ERROR
                     );
-                    return;
+                    std::process::exit(1);
+                }
+                ErrorKind::NotFound => {
+                    eprintln!(
+                        "{}\n\n{}",
+                        "You may have forgotten to enable the selected PWM channel. The \
+                         configuration options to enable either of the two PWM channels are \
+                         listed below.",
+                        PERMISSION_ERROR
+                    );
+                    std::process::exit(1);
                 }
                 _ => panic!("Error: {}", e),
             },
         };
 
     loop {
-        sys.refresh_components();
-        let cpu_temp = get_cpu_temp(&mut sys);
+        let cpu_temp = get_cpu_temp();
         handle_fan_speed(cpu_temp, &mut pwm_pin).expect("Error setting fan speed");
         std::thread::sleep(std::time::Duration::from_secs(5));
     }
